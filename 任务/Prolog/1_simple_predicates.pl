543dfa137399a967cdab3c0101f4ba3e7b5eb48d
li18s <56308532+li18s@users.noreply.github.com>
2019-11-22 12:14:33 +0300

    Update 1_simple_predicates.pl

--- 任务/Prolog/1_simple_predicates.pl
+++ 任务/Prolog/1_simple_predicates.pl
@@ -18,60 +18,37 @@
 */
 
 brother(X,Y):- father(Z,X), father(Z,Y), X \= Y.
-:- brother(X,Y).
 /*
-X = b,
-Y = c ;
-X = c,
-Y = b ;
-X = d,
-Y = e ;
-X = e,
-Y = d ;
+X = b,Y = c ;
+X = c,Y = b ;
+X = d,Y = e ;
+X = e,Y = d ;
 */
 
 cousin(X,Y):- brother(Z,W), father(Z,X), father(W,Y), X \= Y.
-:- cousin(X,Y).
 /*
-X = d,
-Y = f ;
-X = e,
-Y = f ;
-X = f,
-Y = d ;
-X = f,
-Y = e ;
+X = d,Y = f ;
+X = e,Y = f ;
+X = f,Y = d ;
+X = f,Y = e ;
 */
 
 grandson(X,Y):- father(Y,Z), father(Z,X).
-:- grandson(X,Y).
 /*
-X = d,
-Y = a ;
-X = e,
-Y = a ;
-X = f,
-Y = a ;
+X = d,Y = a ;
+X = e,Y = a ;
+X = f,Y = a ;
 */
 
 descendent(X,Y):- father(Y,X).
 descendent(X,Y):- father(Y,Z), descendent(X,Z).
-:- descendent(X,Y).
 /*
-X = b,
-Y = a ;
-X = c,
-Y = a ;
-X = d,
-Y = b ;
-X = e,
-Y = b ;
-X = f,
-Y = c ;
-X = d,
-Y = a ;
-X = e,
-Y = a ;
-X = f,
-Y = a ;
+X = b,Y = a ;
+X = c,Y = a ;
+X = d,Y = b ;
+X = e,Y = b ;
+X = f,Y = c ;
+X = d,Y = a ;
+X = e,Y = a ;
+X = f,Y = a ;
 */
