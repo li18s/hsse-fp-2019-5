4d8213c723088ab14da71172449d07881a77d83e
li18s <56308532+li18s@users.noreply.github.com>
2019-11-22 12:49:24 +0300

    Update 2_truth_table.pl

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,15 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+and(A, B) :- A, B.
+or(A, B) :- A; B.
+xor(A, B) :- not(equ(A, B)).
+not(A)   :- \+ A.
+equ(A, B) :- A = B.
+
+truth_table(A,B,or(A,and(A,B))).
+true true true
+true fail true
+fail true fail
+fail fail fail
