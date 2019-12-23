71ccb3baf540ad490b9e9cf1575353b94e7415b3
li18s <56308532+li18s@users.noreply.github.com>
2019-12-23 11:48:50 +0300

    Update Main.scala

--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -3,4 +3,10 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+  printSet(union(singletonSet(1),singletonSet(2)))
+  printSet(intersect(union(singletonSet(1),singletonSet(2)),union(singletonSet(2),singletonSet(3))))
+  printSet(diff(union(singletonSet(1),singletonSet(2)),singletonSet(1)))
+  printSet(filter(union(singletonSet(1),singletonSet(2)),singletonSet(1)))
+
+ 
 }
