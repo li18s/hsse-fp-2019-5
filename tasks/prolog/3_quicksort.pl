a8fbaa70ddb31bac9cc4965ab4491e7a42ae61d3
li18s <56308532+li18s@users.noreply.github.com>
2019-12-23 11:45:11 +0300

    Update 3_quicksort.pl

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,16 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+/* Sort recursive */
+
+qsort([],[]).
+qsort([Elem], [Elem]).
+qsort([Pivot|Tail], Result):-
+    partition(Pivot, Tail, Smallers, Largers),
+    qsort(Smallers, SortedSmalls),
+    qsort(Largers, SortedLarges),
+    append(SortedSmalls, [Pivot|SortedLarges],Result).
+
+/* Segmentation list */
+
+partition(_,[],[],[]).
+partition(Pivot, [Head|Tail], [Head|Smallers], Largers):- Head =< Pivot, partition(Pivot,  Tail, Smallers, Largers).
+partition(Pivot, [Head|Tail], Smallers, [Head|Largers]) :- Head >= Pivot, partition(Pivot, Tail, Smallers, Largers).
